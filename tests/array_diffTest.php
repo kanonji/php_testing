8f72f905431b4c8b4893f0f0ee1c0efbb32d8f41
kanonji <kanonji@localhost>
2013-08-25 03:06:53 +0900

    array_diff()で添字配列の例をテスト化

--- tests/array_diffTest.php
+++ tests/array_diffTest.php
@@ -8,10 +8,13 @@ class array_diffTest extends PHPUnit_Framework_TestCase{
 		$two = array('a', 'c', 'b');
 		$this->assertEquals(array(), array_diff($one, $two));
 		$one = array('a', 'b', 'c');
-		$two = array('a', 'a', 'a');
-		$this->assertEquals(array('b', 'c'), array_diff($one, $two));
+		$two = array('b', 'b', 'b');
+		$this->assertEquals(array(0 => 'a', 2 => 'c'), array_diff($one, $two));
+		$one = array('a', 'b', 'c');
+		$two = array('a', 'b', 'c', 'd');
+		$this->assertEquals(array(), array_diff($one, $two));
 		$one = array('a', 'b', 'c');
 		$two = array('a', 'b', 'd');
-		$this->assertEquals(array('c', 'd'), array_diff($one, $two));
+		$this->assertEquals(array(2 => 'c'), array_diff($one, $two));
 	}
 }
