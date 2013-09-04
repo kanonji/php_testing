38fb9e28adabb395f3e1d0e1fcfa1611b517e352
kanonji <kanonji@localhost>
2013-09-04 16:34:30 +0900

    privateメソッドをpublicでオーバーライドは出来ないが、オーバーロードっぽい事は出来る

--- tests/objectTest.php
+++ tests/objectTest.php
@@ -30,11 +30,30 @@ class Bar extends Foo{
 		return 'bar';
 	}
 }
+class Baz extends Foo{
+	public function publicAnswer(){
+		return 'baz';
+	}
+	public function protectedAnswer(){
+		return 'baz';
+	}
+	public function privateAnswer(){
+		return 'baz';
+	}
+}
 class objectTest extends PHPUnit_Framework_TestCase{
 	public function test_親クラスのメソッドを通じてオーバーライドしたメソッドを呼ぶ場合、privateなメソッドだけオーバーライド出来ない(){
 		$bar = new Bar();
 		$this->assertEquals('bar', $bar->getPublic());
 		$this->assertEquals('bar', $bar->getProtected());
 		$this->assertEquals('foo', $bar->getPrivate());
+	}
+
+	public function test_privateメソッドをpublicでオーバーライドは出来ないが、オーバーロードっぽい事は出来る(){
+		$baz = new Baz();
+		$this->assertEquals('baz', $baz->getPublic());
+		$this->assertEquals('baz', $baz->getProtected());
+		$this->assertEquals('foo', $baz->getPrivate());
+		$this->assertEquals('baz', $baz->privateAnswer());
 	}
 }
