1a0151c441923b3610fac1d7c653047a2d784a6e
kanonji <kanonji@localhost>
2013-09-04 16:25:15 +0900

    参照渡しされた配列を要素に持つ配列のコピー時の参照配列の扱いについてテスト

--- tests/arrayTest.php
+++ tests/arrayTest.php
@@ -5,9 +5,21 @@ class arrayTest extends PHPUnit_Framework_TestCase{
 		$this->assertTrue(empty($one));
 		$this->assertFalse((bool)$one);
 	}
+
 	public function test_空配列を持つ配列はemptyでfalse(){
 		$one = array(array());
 		$this->assertFalse(empty($one));
 		$this->assertTrue((bool)$one);
+	}
+
+	public function test_配列oneが参照渡しされた配列refを要素に持つ場合、配列oneをコピーしても配列refの参照が切れない(){
+		$ref = array('ref');
+		$one = array(array('foo'));
+		$one[] = &$ref;
+		$two = $one;
+		$two[0][] = 'bar';
+		$two[1][] = 'bar';
+		$this->assertEquals(array(array('foo'), array('ref', 'bar')), $one);
+		$this->assertEquals(array(array('foo', 'bar'), array('ref', 'bar')), $two);
 	}
 }
