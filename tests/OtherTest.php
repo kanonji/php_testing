98d8b19978eb0bda1a3b7bd84d95178b4c2898a6
kanonji <kanonji@users.noreply.github.com>
2014-03-15 14:43:18 +0900

    number_format() can accept float, int, string(number)

--- tests/OtherTest.php
+++ tests/OtherTest.php
@@ -4,4 +4,15 @@ class OtherTest extends PHPUnit_Framework_TestCase{
 	public function testDivide(){
 		$this->assertEquals(7.5, 15 / 2);
 	}
+
+	public function test_number_format(){
+		$val = 1111.49;
+		$this->assertEquals('1,111', number_format($val));
+		$this->assertEquals('1,111', number_format((int)$val));
+		$this->assertEquals('1,111', number_format((string)$val));
+		$val = 1111.5;
+		$this->assertEquals('1,112', number_format($val));
+		$this->assertEquals('1,111', number_format((int)$val));
+		$this->assertEquals('1,112', number_format((string)$val));
+	}
 }
