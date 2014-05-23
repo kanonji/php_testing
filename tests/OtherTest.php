fdbd669b5df8ac3f4100c75795c9af35204167e9
kanonji <kanonji@users.noreply.github.com>
2014-05-23 18:18:18 +0900

    nullが入ってる変数に配列アクセスしてもUndefined index noticeは出ない

--- tests/OtherTest.php
+++ tests/OtherTest.php
@@ -15,4 +15,17 @@ class OtherTest extends PHPUnit_Framework_TestCase{
         $this->assertEquals('1,111', number_format((int)$val));
         $this->assertEquals('1,112', number_format((string)$val));
     }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function test_undefined_index(){
+        $ary = array();
+        $ary['foo'];
+    }
+
+    public function test_no_undefined_index(){
+        $var = null;
+        $var['foo'];
+    }
 }
