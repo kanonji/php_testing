50f2f92608e027d5fcf7dbc2baafde33b61a09b0
kanonji <kanonji@users.noreply.github.com>
2014-10-02 12:14:22 +0900

    date_parse()は日時のバリデーションとしては穴が多い

--- tests/DateTimeTest.php
+++ tests/DateTimeTest.php
@@ -41,4 +41,59 @@ class DateTimeTest extends PHPUnit_Framework_TestCase{
         $interval = $base->diff($comparative);
         $this->assertEquals('1 days', $interval->format('%d days'));
     }
+
+    public function test_date_parseは1文字の文字列でjを除くa〜zを渡してもエラーにならない(){
+        $letters = array_merge(range('A','Z'), range('a','z'));
+        foreach($letters as $ascii){
+            $result = date_parse($ascii);
+            if('j' === strtolower($ascii)){
+                $this->assertEquals(1, $result['error_count']);
+                $this->assertEquals(array('The timezone could not be found in the database'), $result['errors']);
+                $this->assertEquals(0, $result['warning_count']);
+                $this->assertEquals(array(), $result['warnings']);
+            } else {
+                $this->assertEquals(0, $result['error_count']);
+                $this->assertEquals(array(), $result['errors']);
+                $this->assertEquals(0, $result['warning_count']);
+                $this->assertEquals(array(), $result['warnings']);
+            }
+        }
+    }
+
+    public function test_date_parseは3桁までの数字は全てエラーになる(){
+        foreach(range(0, 999) as $num){
+            $numbers[] = $num;
+            $numbers[] = (string)$num;
+        }
+        foreach($numbers as $num){
+            $result = date_parse($num);
+            $count = strlen($num);
+            $errorsExpected = array_fill(0, $count, 'Unexpected character');
+            $this->assertEquals($count, $result['error_count']);
+            $this->assertEquals($errorsExpected, $result['errors']);
+            $this->assertEquals(0, $result['warning_count']);
+            $this->assertEquals(array(), $result['warnings']);
+        }
+    }
+
+    public function test_date_parseは4桁数字はほぼエラーにならない(){
+        foreach(range(1000, 9999) as $num){
+            $numbers[] = $num;
+            $numbers[] = (string)$num;
+        }
+        foreach($numbers as $num){
+            $result = date_parse($num);
+            if (2400 <= $num && $num < 2460) {
+                $this->assertEquals(0, $result['error_count']);
+                $this->assertEquals(array(), $result['errors']);
+                $this->assertEquals(1, $result['warning_count']);
+                $this->assertEquals(array(5 => 'The parsed time was invalid'), $result['warnings']);
+            } else {
+                $this->assertEquals(0, $result['error_count']);
+                $this->assertEquals(array(), $result['errors']);
+                $this->assertEquals(0, $result['warning_count']);
+                $this->assertEquals(array(), $result['warnings']);
+            }
+        }
+    }
 }
