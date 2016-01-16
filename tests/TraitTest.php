889700e3f8b1d5ad379deb8e6e8b480425502a0f
kanonji <kanonji@users.noreply.github.com>
2016-01-17 04:09:03 +0900

    5.3をテスト対象から除外

--- tests/TraitTest.php
+++ tests/TraitTest.php
@@ -1,6 +1,4 @@
 <?php
-if( version_compare(PHP_VERSION, '5.4.0') >= 0 ){
-
 trait FooTrait{
     public function fooPublic(){
         return get_class_methods(__trait__);
@@ -87,6 +85,4 @@ class TraitTest extends PHPUnit_Framework_TestCase{
         $this->assertEquals('BarTraitClass', $obj->get__class__());
         $this->assertEquals('BarTrait', $obj->get__trait__());
     }
-}
-
 }
