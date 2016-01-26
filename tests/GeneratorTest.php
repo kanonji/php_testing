dfd4b68b78e621ff4dffdb765bf34c538d1f59bb
kanonji <kanonji@users.noreply.github.com>
2016-01-27 04:50:00 +0900

    1度でもnext()を呼んだGeneratorはrewind()出来ない

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -21,4 +21,21 @@ class GeneratorTest extends PHPUnit_Framework_TestCase{
             $this->assertTrue($generator instanceof Iterator);
         }
     }
+
+    public function test_1度でもnext（）を呼んだGeneratorはrewind（）出来ない(){
+        if(function_exists('getGenerator')){
+            $generator = getGenerator();
+            $generator->rewind();
+            $this->assertEquals(1, $generator->current());
+            $generator->rewind();
+            $generator->next();
+            $this->assertEquals(2, $generator->current());
+            try{
+                $generator->rewind();
+            } catch(Exception $e) {
+                $errorMessage = 'Cannot rewind a generator that was already run';
+                $this->assertEquals($errorMessage, $e->getMessage());
+            }
+        }
+    }
 }
