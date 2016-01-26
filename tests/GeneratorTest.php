445f896743c21d6fc867c5983fbe56e42cb32339
kanonji <kanonji@users.noreply.github.com>
2016-01-27 04:43:55 +0900

    Generator is Iterator

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -1,13 +1,24 @@
 <?php
+if(version_compare(PHP_VERSION, '5.5.0', '>=')){
+     eval('function getGenerator(){
+        yield 1;
+        yield 2;
+        yield 3;
+    }');
+}
+
 class GeneratorTest extends PHPUnit_Framework_TestCase{
     public function test_Generator_is_Traversable(){
-        if(version_compare(PHP_VERSION, '5.5.0', '>=')){
-             eval('$generator = current([function(){
-                yield 1;
-                yield 2;
-                yield 3;
-            }])->__invoke();');
+        if(function_exists('getGenerator')){
+            $generator = getGenerator();
             $this->assertTrue($generator instanceof Traversable);
+        }
+    }
+
+    public function test_Generator_is_Iterator(){
+        if(function_exists('getGenerator')){
+            $generator = getGenerator();
+            $this->assertTrue($generator instanceof Iterator);
         }
     }
 }
